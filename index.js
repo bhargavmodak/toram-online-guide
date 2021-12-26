67bb845e04d71546c8e0dee7c5492f5e81334b4a
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2021-12-26 16:40:58 +0530

    Make navbar push content

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+function sidebar() {
+  if ($(".mobile-indicator").css('display') == 'block') {
+    if ($(".nav-open").prop('checked')) {
+      $(".sidenav").css('width','100%');
+    } else {
+      $(".sidenav").css('width', 0);
+    }
+  } else {
+    if ($(".nav-open").prop('checked')) {
+      $(".sidenav").css('width','15rem');
+      $(".main, .hero-onfrost").css('width','calc(100% - 15rem)');
+      $(".main").css('margin-left','15rem');
+    } else {
+      $(".sidenav").css('width', 0);
+      $(".main, .hero-onfrost").css('width','100%');
+      $(".main").css('margin-left','0');
+    }
+  }
+}
+
+$(".nav-btn").click(sidebar);
+
 $(".sidenav a").click(function () {
-  $(".nav-open").prop('checked', false);
-})
+  if ($(".mobile-indicator").css('display') == 'block') {
+    $(".nav-open").prop('checked',false);
+  }
+  sidebar();
+});
