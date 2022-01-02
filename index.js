490e6c99d9ecac5c796f09e1e15518b42e4335bc
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2022-01-02 23:41:54 +0530

    Fix Dark/Light mode

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-function sidebar() {
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const currentTheme = localStorage.getItem("theme");
+
+// Function to set theme as theme
+if (prefersDarkScheme.matches && currentTheme == "dark") {
+  $("html").attr('data-theme', 'dark');
+  $("body").css('background','url(images/protruding-squares-dark.svg)');
+} else {
+  $("html").attr('data-theme', 'light');
+  $("body").css('background','url(images/protruding-squares.svg)');
+}
+
+// A function to toggle the sidebar
+function toggleSidebar() {
+  // For mobile screens
   if ($(".mobile-indicator").css('display') == 'block') {
     if ($(".nav-open").prop('checked')) {
       $(".sidenav").css('width','100%');
@@ -6,6 +20,7 @@ function sidebar() {
       $(".sidenav").css('width', 0);
     }
   } else {
+    // For non mobile screens
     if ($(".nav-open").prop('checked')) {
       $(".sidenav").css('width','15rem');
       $(".main, .hero-onfrost").css('width','calc(100% - 15rem)');
@@ -18,23 +33,28 @@ function sidebar() {
   }
 }
 
-$(".nav-btn").click(sidebar);
+$(".nav-btn").click(toggleSidebar);
 
 $(".sidenav a").click(function () {
+  // Close navbar if link is clicked on a mobile device
   if ($(".mobile-indicator").css('display') == 'block') {
     $(".nav-open").prop('checked',false);
   }
-  sidebar();
+  toggleSidebar();
 });
 
+// Changing the theme
 $(".toggle-icon").click(function () {
   $(".toggle-icon").toggleClass('fa-sun');
   $(".toggle-icon").toggleClass('fa-moon');
+  let theme = "light";
   if ($("html").attr('data-theme') == 'dark') {
     $("html").attr('data-theme', 'light');
-    $("body").css('background','url(images/protruding-squares.svg)')
+    $("body").css('background','url(images/protruding-squares.svg)');
   } else {
     $("html").attr('data-theme', 'dark');
-    $("body").css('background','url(images/protruding-squares-dark.svg)')
+    $("body").css('background','url(images/protruding-squares-dark.svg)');
+    theme = "dark";
   }
+  localStorage.setItem("theme", theme);
 })
